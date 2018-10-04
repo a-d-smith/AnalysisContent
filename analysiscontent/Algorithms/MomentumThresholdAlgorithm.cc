4f4f32a9dbc645ddb38b075ceab77c34ba1e53bf
a-d-smith <andrewsmithweb@gmail.com>
2018-10-04 15:56:47 -0500

    Momentum threshold algorithm now outputs to tree

--- analysiscontent/Algorithms/MomentumThresholdAlgorithm.cc
+++ analysiscontent/Algorithms/MomentumThresholdAlgorithm.cc
@@ -7,6 +7,9 @@
 #include "analysiscontent/Algorithms/MomentumThresholdAlgorithm.h"
 
 #include "Pandora/AlgorithmHeaders.h"
+#include "Pandora/PdgTable.h"
+
+#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
 
 #include <iostream>
 
@@ -17,20 +20,131 @@ namespace analysis_content
 
 MomentumThresholdAlgorithm::MomentumThresholdAlgorithm()
 {
-    std::cout << "I have been created!" << std::endl;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+MomentumThresholdAlgorithm::~MomentumThresholdAlgorithm()
+{
+    PANDORA_MONITORING_API(SaveTree(this->GetPandora(), m_treeName.c_str(), m_fileName.c_str(), "UPDATE"));
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 StatusCode MomentumThresholdAlgorithm::Run()
 {
+    // Get the input collections
+	const MCParticleList *pMCParticleList = nullptr;
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_mcParticleListName, pMCParticleList));
+
+	const CaloHitList *pCaloHitList = nullptr;
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_caloHitListName, pCaloHitList));
+
+    // Get the neutrino final state MCParticles
+	LArMCParticleHelper::MCContributionMap mcParticleToHitsMap;
+	MCParticleVector finalStateParticles;
+	this->GetNeutrinoFinalStates(pMCParticleList, pCaloHitList, finalStateParticles, mcParticleToHitsMap);
+
+    // Determine which are reconstructable
+	MCParticleToBoolMap mcParticleToIsReconstrutableMap;
+	this->IdentifyReconstructableMCParticles(pMCParticleList, pCaloHitList, finalStateParticles, mcParticleToIsReconstrutableMap);
+
+    // Output the details to a tree
+    for (const auto &pMCParticle : finalStateParticles)
+    {
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "pdg", pMCParticle->GetParticleId()));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "momentum", pMCParticle->GetMomentum().GetMagnitude()));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "energy", pMCParticle->GetEnergy()));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "mass", PdgTable::GetParticleMass(pMCParticle->GetParticleId())));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nHitsU", this->CountHits(pMCParticle, mcParticleToHitsMap, TPC_VIEW_U)));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nHitsV", this->CountHits(pMCParticle, mcParticleToHitsMap, TPC_VIEW_V)));
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "nHitsW", this->CountHits(pMCParticle, mcParticleToHitsMap, TPC_VIEW_W)));
+
+        const auto it = mcParticleToIsReconstrutableMap.find(pMCParticle);
+        if (it == mcParticleToIsReconstrutableMap.end())
+            throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+        PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), m_treeName.c_str(), "isReconstructable", static_cast<int>(it->second)));
+        PANDORA_MONITORING_API(FillTree(this->GetPandora(), m_treeName.c_str()));
+    }
+
     return STATUS_CODE_SUCCESS;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-StatusCode MomentumThresholdAlgorithm::ReadSettings(const TiXmlHandle /*xmlHandle*/)
+void MomentumThresholdAlgorithm::GetNeutrinoFinalStates(const MCParticleList *pMCParticleList, const CaloHitList *pCaloHitList, 
+    MCParticleVector &finalStateParticles, LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap) const
 {
+	if (!finalStateParticles.empty())
+		throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+	
+	if (!mcParticleToHitsMap.empty())
+		throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+	LArMCParticleHelper::PrimaryParameters paramsAll;
+	paramsAll.m_minPrimaryGoodHits = 0;
+	paramsAll.m_minHitsForGoodView = 0;
+	paramsAll.m_minHitSharingFraction = 0.f;
+
+	LArMCParticleHelper::SelectReconstructableMCParticles(pMCParticleList, pCaloHitList, paramsAll, LArMCParticleHelper::IsBeamNeutrinoFinalState, mcParticleToHitsMap);
+	this->GetOrderedMCParticleVector(mcParticleToHitsMap, finalStateParticles);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void MomentumThresholdAlgorithm::IdentifyReconstructableMCParticles(const MCParticleList *pMCParticleList, const CaloHitList *pCaloHitList,
+    const MCParticleVector &finalStateParticles, MCParticleToBoolMap &mcParticleToIsReconstrutableMap) const
+{
+	if (!mcParticleToIsReconstrutableMap.empty())
+		throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+	LArMCParticleHelper::PrimaryParameters params;
+	LArMCParticleHelper::MCContributionMap reconstructableMCParticleToHitsMap;
+	LArMCParticleHelper::SelectReconstructableMCParticles(pMCParticleList, pCaloHitList, params, LArMCParticleHelper::IsBeamNeutrinoFinalState, reconstructableMCParticleToHitsMap);
+
+	for (const auto &pMCParticle : finalStateParticles)
+		mcParticleToIsReconstrutableMap.emplace(pMCParticle, reconstructableMCParticleToHitsMap.find(pMCParticle) != reconstructableMCParticleToHitsMap.end());
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void MomentumThresholdAlgorithm::GetOrderedMCParticleVector(const LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap,
+	MCParticleVector &finalStateParticles) const
+{
+	for (const auto &entry : mcParticleToHitsMap)
+		finalStateParticles.push_back(entry.first);
+
+	// Sort using the built in < operator
+	std::sort(finalStateParticles.begin(), finalStateParticles.end());
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+int MomentumThresholdAlgorithm::CountHits(const MCParticle *const pMCParticle,
+    const LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap, const HitType &view) const
+{
+    const auto it = mcParticleToHitsMap.find(pMCParticle);
+    if (it == mcParticleToHitsMap.end())
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    int nHits = 0;
+    for (const auto &pCaloHit : it->second)
+        nHits += (pCaloHit->GetHitType() == view) ? 1 : 0;
+
+    return nHits;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode MomentumThresholdAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "CaloHitListName", m_caloHitListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "MCParticleListName", m_mcParticleListName));
+    
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputTree", m_treeName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputFile", m_fileName));
+
     return STATUS_CODE_SUCCESS;
 }
 
--- analysiscontent/Algorithms/MomentumThresholdAlgorithm.h
+++ analysiscontent/Algorithms/MomentumThresholdAlgorithm.h
@@ -9,6 +9,8 @@
 
 #include "Pandora/Algorithm.h"
 
+#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
+
 namespace analysis_content
 {
 
@@ -16,12 +18,35 @@ class MomentumThresholdAlgorithm : public pandora::Algorithm
 {
 public:
     MomentumThresholdAlgorithm();
+    ~MomentumThresholdAlgorithm();
 
 protected:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
     pandora::StatusCode Run();
+
+private:
+    typedef std::unordered_map<const pandora::MCParticle *, bool> MCParticleToBoolMap;
+    typedef lar_content::LArMCParticleHelper LArMCParticleHelper;
+
+    void GetNeutrinoFinalStates(const pandora::MCParticleList *pMCParticleList, const pandora::CaloHitList *pCaloHitList,
+        pandora::MCParticleVector &finalStateParticles, LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap) const;
+
+    void IdentifyReconstructableMCParticles(const pandora::MCParticleList *pMCParticleList,	const pandora::CaloHitList *pCaloHitList,
+        const pandora::MCParticleVector &finalStateParticles, MCParticleToBoolMap &mcParticleToIsReconstrutableMap) const;
+
+    void GetOrderedMCParticleVector(const LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap,
+        pandora::MCParticleVector &finalStateParticles) const;
+
+    int CountHits(const pandora::MCParticle *const pMCParticle, const LArMCParticleHelper::MCContributionMap &mcParticleToHitsMap,
+        const pandora::HitType &view) const;
+
+    std::string  m_caloHitListName;
+    std::string  m_mcParticleListName;
+
+    std::string  m_treeName;
+    std::string  m_fileName;
 };
 
-} // namespace analysis_content
+} // namespace analysis_content1
 
 #endif // MOMENTUM_THRESHOLD_ALGORITHM
